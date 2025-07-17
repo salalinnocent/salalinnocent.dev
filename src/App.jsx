4807bb86e61f860021398ba4868e31f365189937
Salal Innocent <88629866+Badman47@users.noreply.github.com>
2025-07-17 18:38:41 +0500

    Integrated vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import PageLayout from "./PageLayout/PageLayout"
+import { Analytics } from "@vercel/analytics/next"
 //Main App Instance
 const App = ({ children }) => {
   return (
     <>
       <PageLayout children={children} />
+      <Analytics/>
+
     </>
   )
 }
